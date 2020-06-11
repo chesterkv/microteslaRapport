4e179b201aa8901ecc1658ad9af4639621109f1e
ayachm <66802725+ayachm@users.noreply.github.com>
2020-06-11 22:56:04 +0200

    Update test_ultrasensor.py

--- test_ultrasensor.py
+++ test_ultrasensor.py
@@ -13,9 +13,11 @@ gpio.setup(echo,gpio.IN)
 stop = 1
 v = 34000 #vitesse du son en cm.s-1
 while stop != 0:
+	#on met le trigger à HIGH pendant 10 microsecondes
 	gpio.output(trig,True)
 	time.sleep(0.00001)
 	gpio.output(trig,False)
+	#on récupère la distance en comptant le temps que met le signal à revenir
 	start = time.time()
 	while gpio.input(echo) == False:
 		start = time.time()
